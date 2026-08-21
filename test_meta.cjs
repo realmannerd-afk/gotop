ae67e6e8791702b36dbffe0d5e4afde658105063
santhoshkumar-repo <santhosh.g@nextitpoint.com>
2026-08-22 00:52:33 +0530

    Remove temp files

--- test_meta.cjs
+++
@@ -1,5 +0,0 @@
-const { fetchMetadata } = require('./src/lib/metadata.js');
-async function run() {
-  console.log(await fetchMetadata('https://mindphor.com'));
-}
-run();
--- test_meta.ts
+++
@@ -1,7 +0,0 @@
-import { fetchMetadata } from "./src/lib/metadata";
-async function run() {
-  console.log("Fetching mindphor.com...");
-  const res = await fetchMetadata("https://mindphor.com");
-  console.log("Result:", res);
-}
-run();
