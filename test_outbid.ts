031cab0871c9a8da2876f89a703e9e967f413308
santhoshkumar-repo <santhosh.g@nextitpoint.com>
2026-08-22 01:01:36 +0530

    Remove test_outbid.ts

--- test_outbid.ts
+++
@@ -1,7 +0,0 @@
-import { fetchMetadata } from "./src/lib/metadata";
-async function run() {
-  console.log("Fetching outbid.lol...");
-  const res = await fetchMetadata("https://outbid.lol");
-  console.log("Result:", res);
-}
-run();
